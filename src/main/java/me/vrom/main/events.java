0556a489f15ca9848e4e712d0330fd42650a4ff8
AarushKiller <67967009+AarushKiller@users.noreply.github.com>
2022-01-25 19:29:00 +0545

    stuff

--- src/main/java/me/vrom/main/events.java
+++ src/main/java/me/vrom/main/events.java
@@ -1,16 +1,20 @@
 package me.vrom.main;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.Objects;
+
+
 public class events implements Listener {
+    @EventHandler
     public void onDamageTake (EntityDamageEvent event) {
         Player player = (Player) event.getEntity();
         if(!player.hasPlayedBefore()) {
-            
         }
     }
 }
